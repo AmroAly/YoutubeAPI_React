766ce3227d36aabab79c36c63d6bb4af1e88c0dd
Amr Aly <amr.aly57@yahoo.com>
2017-08-27 04:54:17 +0200

    remove my api key

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
-const API_KEY = 'AIzaSyA0IeIwLj1P9gOhdePvKhPR-WexyriF1ho';
+const API_KEY = '';
 
 
 
